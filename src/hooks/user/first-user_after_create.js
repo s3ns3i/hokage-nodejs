acd249aea9eb051501092b04071392caa7dd2bcb
s3ns3i <eragonsm@gmail.com>
2020-04-25 02:20:41 +0200

    Added Uploader role

---
+++ src/hooks/user/first-user_after_create.js
@@ -0,0 +1,12 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (options = {}) => {
+  return async context => {
+    try {
+      await context.app.service('role').create({name: 'Uploader'});
+      return context;
+    } catch (error) { throw new Error(error); }
+  };
+};
