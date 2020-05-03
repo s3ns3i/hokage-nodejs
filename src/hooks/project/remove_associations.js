d2c120b9cf0b0c88e28ab1ad1d2c495b98946160
s3ns3i <eragonsm@gmail.com>
2020-05-03 23:01:26 +0200

    Fixed project relationships hooks

--- src/hooks/project/remove_associations.js
+++ src/hooks/project/remove_associations.js
@@ -1,30 +1,25 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-const { Sequelize } = require('sequelize');
-
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async context => {
     if (context.data.project_roles) {
-      const sequelizeClient = context.app.settings.sequelizeClient;
-      const ProjectRole = sequelizeClient.models['project_role'];
-      const UserProjectRole = sequelizeClient.models['user_project_role'];
 
       try {
         const project = await context.app.service('project').get(context.id);
 
         let projectRoleIds = await context.app.service('project-role')
-          .find({ where: { projectId: project.id } });
+          .find({ query: { projectId: project.id } });
         projectRoleIds = projectRoleIds.data
           .map(projectRole => projectRole.id);
+        console.log(projectRoleIds);
 
-        const { in: opIn } = Sequelize.Op;
-        await UserProjectRole
-          .destroy({ where: { projectRoleId: { [opIn]: projectRoleIds } } });
+        await context.app.service('user-project-role')
+          .remove(null, { query: { projectRoleId: { $in: projectRoleIds } } });
 
-        await ProjectRole
-          .destroy({ where: { projectId: project.id } });
+        await context.app.service('project-role')
+          .remove(null, { query: { projectId: project.id } });
 
         return context;
       } catch (error) { throw new Error(error); }
