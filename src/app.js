984b9efe90ec4e2f73353fa19f9b7f509f9bfa08
s3ns3i <eragonsm@gmail.com>
2020-09-03 00:04:32 +0200

    Removed unused imports from app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 require('./polyfills/array_flat');
-const path = require('path');
-const favicon = require('serve-favicon');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
