fd33b106adeeeb0ee2aea167598f9ed449245a2c
s3ns3i <eragonsm@gmail.com>
2020-09-10 20:37:51 +0200

    App uses certificates to handle https requests

--- src/index.js
+++ src/index.js
@@ -3,7 +3,43 @@ const logger = require('./logger');
 const app = require('./app');
 const hostname = app.get('host');
 const port = app.get('port');
-const server = app.listen(port, hostname);
+
+let server = null;
+if(process.env.NODE_ENV) {
+  const https = require('https');
+  const fs = require('fs');
+  const path = require('path');
+  const socketio = require('@feathersjs/socketio');
+
+  app.configure(socketio());
+
+  server = https.createServer({
+    key: fs.readFileSync(
+      path.resolve(
+        '../ssl/keys',
+        '969cd_c3acb_e02d5936dbece98a4c9ebb23f6453167.key'
+      )
+    ),
+    cert: fs.readFileSync(
+      path.resolve(
+        '../ssl/certs',
+        'hokage_onepiecenakama_pl_969cd_c3acb_1607212799_0e3e7effb14cbe406076db0b710bba56.crt'
+      )
+    )
+  }, app).listen(port, hostname);
+
+  app.setup(server);
+
+  server.on('listening', () =>
+    logger.info('Feathers application started on https://%s:%d', hostname, port)
+  );
+} else {
+  server = app.listen(port, hostname);
+
+  server.on('listening', () =>
+    logger.info('Feathers application started on http://%s:%d', hostname, port)
+  );
+}
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
@@ -12,7 +48,3 @@ process.on('unhandledRejection', (reason, p) =>
 process.on('uncaughtException', (error) => {
   logger.error(error);
 });
-
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', hostname, port)
-);
